f088372fc2a6b8d4ab66b677c52889fb2e7c3368
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-05-11 12:19:10 +0200

    added one more plot

--- plot.py
+++ plot.py
@@ -2,8 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-rewards = "partA_results/1683796437/group_06_catch_rewards_1683796437.npy"
-losses = "losses_1683496002.npy"
+rewards = "partA_results/5/group_06_catch_rewards_5.npy"
 
 arr = np.load(rewards, allow_pickle=True)
 plt.plot(arr)
