61b8b4412f93a091df3f5e4a370e219370c353ac
o.t.ozyilmaz <o.t.ozyilmaz@student.rug.nl>
2023-05-24 15:22:38 +0200

    working training

--- partB.py
+++ partB.py
@@ -1,34 +1,38 @@
 import gym
 from stable_baselines3 import A2C
-from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.env_util import make_atari_env, make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import ts2xy, plot_results
 from gym.spaces import Box
 from tqdm import tqdm
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 # Create the Atari environment
-env = make_atari_env('ALE/Skiing-v5', n_envs=1, seed=0)
+# env = make_atari_env('LunarLander-v2', n_envs=1, seed=0)
+env = gym.make('ALE/Assault-v5')
+env = Monitor(env, "logdir/")
 
 # Create and train the SAC agent
-model = A2C('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=5000)
-
-# Evaluate the trained agent
-obs = env.reset()
-rewards = []
-r2 = []
-for i in tqdm(range(1000)):
-    action, _ = model.predict(obs)
-    action = int(action)  # Convert the action to an integer
-    obs, reward, done, info = env.step([int(action)])
-    rewards.append(reward)
-    if done:
-        r2.append(reward)
-        obs = env.reset()
-
-print(rewards, r2)
-print(np.mean(rewards))
-print(np.mean(r2))
+model = A2C('CnnPolicy', env, verbose=1)
+rewards_history = model.learn(total_timesteps=50000, progress_bar=True)
+# # Evaluate the trained agent
+# obs = env.reset()
 # mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=100)
 # print(f"Mean reward: {mean_reward:.2f}")
+
+# # Plot the reward and loss
+plot_results(["logdir/"], 50000, results_plotter.X_TIMESTEPS, "ALE/Assault-v5")
+plt.show()
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    if done:
+        obs = env.reset()
+    env.render()
+    time.sleep(.01)
