1082b71bd4f175144fe2cf84a11ed6ca81ef552c
o.t.ozyilmaz <o.t.ozyilmaz@student.rug.nl>
2023-05-27 10:41:38 +0200

    tensorboard + frame stacking

--- partB.py
+++ partB.py
@@ -1,11 +1,12 @@
 import gym
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO, DDPG, TD3, SAC
 from stable_baselines3.common.env_util import make_atari_env, make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from stable_baselines3.common import results_plotter
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.results_plotter import ts2xy, plot_results
+from stable_baselines3.common.env_util import make_vec_env
 from gym.spaces import Box
 from tqdm import tqdm
 import numpy as np
@@ -13,26 +14,11 @@ import matplotlib.pyplot as plt
 import time
 
 # Create the Atari environment
-# env = make_atari_env('LunarLander-v2', n_envs=1, seed=0)
-env = gym.make('ALE/Assault-v5')
-env = Monitor(env, "logdir/")
+env = gym.make('Breakout-v0')
+env = make_vec_env('Breakout-v0', n_envs=4)
+env = VecFrameStack(env, n_stack=4)
 
 # Create and train the SAC agent
-model = A2C('CnnPolicy', env, verbose=1)
-rewards_history = model.learn(total_timesteps=50000, progress_bar=True)
-# # Evaluate the trained agent
-# obs = env.reset()
-# mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=100)
-# print(f"Mean reward: {mean_reward:.2f}")
-
-# # Plot the reward and loss
-plot_results(["logdir/"], 50000, results_plotter.X_TIMESTEPS, "ALE/Assault-v5")
-plt.show()
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    if done:
-        obs = env.reset()
-    env.render()
-    time.sleep(.01)
+model = A2C('CnnPolicy', env, verbose=1, tensorboard_log="logdir/", n_steps=75, learning_rate=0.0007, gamma=0.99, gae_lambda=0.95, vf_coef=0.5, ent_coef=0.01, max_grad_norm=0.5, rms_prop_eps=1e-5, use_rms_prop=True, use_sde=False, sde_sample_freq=-1, normalize_advantage=False, create_eval_env=False, policy_kwargs=None, device='auto', _init_setup_model=True)
+rewards_history = model.learn(total_timesteps=10000000)
+model.save("ppo_breakout")
