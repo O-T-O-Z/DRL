e1d79cf6986427c8b9345454e082b828cf404055
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-05-29 15:04:15 +0200

    new eval method

--- partB.py
+++ partB.py
@@ -5,6 +5,8 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.env_util import make_vec_env
 import numpy as np
+from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, VecMonitor, is_vecenv_wrapped
+from stable_baselines3.common.monitor import Monitor
 
 
 STATUS = "eval"
@@ -34,37 +36,74 @@ if __name__ == "__main__":
         model, env = get_model()
         model = model.load(MODEL_NAME)
         observation = env.reset()
-        
-        all_rewards = [[] for i in range(4)]
-        game_means = []
-        game_lens = []
+        random = False
 
-        while True:
-            if len(game_means) == 100:
-                break
+        ## Code below has been adapted from https://github.com/DLR-RM/stable-baselines3/blob/master/stable_baselines3/common/evaluation.py#L11
 
-            #action, _ = model.predict(observation)
-            action = [env.action_space.sample() for x in range(4)]
+        episode_rewards = []
+        episode_lengths = []
+        is_monitor_wrapped = is_vecenv_wrapped(env, VecMonitor) or env.env_is_wrapped(Monitor)[0]
+        n_envs = 4
+        n_eval_episodes = 100
 
-            observation, reward, done, info = env.step(action)
+        episode_counts = np.zeros(n_envs, dtype="int")
+        episode_count_targets = np.array([(n_eval_episodes + i) // n_envs for i in range(n_envs)], dtype="int")
 
-            for i, r in enumerate(reward):
-                all_rewards[i].append(r)
+        current_rewards = np.zeros(n_envs)
+        current_lengths = np.zeros(n_envs, dtype="int")
+        observations = env.reset()
+        states = None
+        episode_starts = np.ones((env.num_envs,), dtype=bool)
+        while (episode_counts < episode_count_targets).any():
 
-            if done.any():
-                trues = np.where(done)[0]
-                for idx in trues:
-                    m = np.mean(all_rewards[idx])
-                    print(m)
-                    game_lens.append(len(all_rewards[idx]))
-                    all_rewards[idx] = []
-                    game_means.append(m)
-                print(len(game_means))
+            if not random:
+                actions, states = model.predict(
+                    observations,
+                    state=states,
+                    episode_start=episode_starts,
+                    deterministic=False,
+                )
+            else:
+                actions = [env.action_space.sample() for x in range(4)]
+
+            new_observations, rewards, dones, infos = env.step(actions)
+            current_rewards += rewards
+            current_lengths += 1
+            for i in range(n_envs):
+                if episode_counts[i] < episode_count_targets[i]:
+                    # unpack values so that the callback can access the local variables
+                    reward = rewards[i]
+                    done = dones[i]
+                    info = infos[i]
+                    episode_starts[i] = done
+
+                    if dones[i]:
+                        print(current_rewards[i])
+                        if is_monitor_wrapped:
+                            # Atari wrapper can send a "done" signal when
+                            # the agent loses a life, but it does not correspond
+                            # to the true end of episode
+                            if "episode" in info.keys():
+                                # Do not trust "done" with episode endings.
+                                # Monitor wrapper includes "episode" key in info if environment
+                                # has been wrapped with it. Use those rewards instead.
+                                episode_rewards.append(info["episode"]["r"])
+                                episode_lengths.append(info["episode"]["l"])
+                                # Only increment at the real end of an episode
+                                episode_counts[i] += 1
+                        else:
+                            episode_rewards.append(current_rewards[i])
+                            episode_lengths.append(current_lengths[i])
+                            episode_counts[i] += 1
+                        current_rewards[i] = 0
+                        current_lengths[i] = 0
+
+            observations = new_observations
 
             env.render("human")
 
-        print(game_lens)
-        print(np.mean(game_lens))
-        print(game_means)
-        print(np.mean(game_means))
+        mean_reward = np.mean(episode_rewards)
+        std_reward = np.std(episode_rewards)
+        print(episode_rewards)
+        print(mean_reward)
 
--- plotB.py
+++ plotB.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import seaborn as sns
 
 folder = "partB_results"
 file = os.path.join(folder, "20m_pitfall.csv")
@@ -18,6 +20,36 @@ data = np.array(data)
 plt.plot(data[:,1] / 1000000, data[:,2])
 plt.xlabel("Timestep (in millions)")
 plt.ylabel("Average Reward")
-plt.title("Pitfall! performance over 1 run")
+plt.title("Pitfall! training performance over 1 run")
 plt.savefig(os.path.join(folder, "rewards_pitfall.pdf"), bbox_inches="tight")
+plt.show()
+plt.cla()
+
+
+eval_file = "model_vs_random.txt"
+
+data_eval = []
+data_means = []
+
+with open(os.path.join(folder, eval_file), "r") as f:
+    for line in f.readlines():
+        row = [float(x) for x in line.split(",")]
+        data_eval.append(row)
+        data_means.append(np.mean(row))
+
+
+data_eval = pd.DataFrame({"A2C Model": data_eval[0], "Random": data_eval[1]})
+
+my_colors = {'A2C Model': 'blue', 'Random': 'skyblue'}
+ax = sns.boxplot(data=data_eval, palette=my_colors)
+
+
+
+plt.ylabel("Average Reward")
+
+plt.axhline(y=data_means[0], linestyle='--', color="green", label="A2C Model mean = " + str(data_means[0]), alpha=0.7)
+plt.axhline(y=data_means[1], linestyle='--', color="dodgerblue", label="Random mean = " + str(data_means[1]), alpha=0.7)
+plt.title('Pitfall! testing performance on 100 episodes')
+plt.legend()
+plt.savefig(os.path.join(folder, "eval_pitfall.pdf"), bbox_inches="tight")
 plt.show()
