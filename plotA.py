a89926330e1dd46616f5b8a2161fe2f0adbb2af6
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-05-29 11:24:31 +0200

    better plots

--- plotA.py
+++ plotA.py
@@ -2,13 +2,25 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = os.path.join("partA_results", "4")
-file = "group_06_catch_rewards_4.npy"
+folder = "partA_results"
 
-arr = np.load(os.path.join(folder, file), allow_pickle=True)
-plt.plot(np.arange(0, len(arr)) * 10, arr)
+data = []
+
+for i in range(1, 6):
+    path = os.path.join(folder, str(i), f"group_06_catch_rewards_{i}.npy")
+    data.append(np.load(path, allow_pickle=True))
+
+data = np.array(data)
+
+min_ = data.min(axis=0)
+max_ = data.max(axis=0)
+mean = data.mean(axis=0)
+x = np.arange(0, len(mean)) * 10
+
+plt.plot(x, mean)
+plt.fill_between(x, min_, max_, alpha = 0.3)
 plt.xlabel("Testing Episode")
 plt.ylabel("Average Reward")
-plt.title("Catch Performance")
+plt.title("Catch performance over 5 runs")
 plt.savefig(os.path.join(folder, "rewards_catch.pdf"), bbox_inches="tight")
 plt.show()
